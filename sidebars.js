229a5f55da153840cd21e8cda94cf0ea71906889
Document Daemon <hello@masternode.io>
2021-07-30 08:15:28 -0700

    Ok I think I found it manually forgot the sidebar file it was referencing some stale ID's

--- sidebars.js
+++ sidebars.js
@@ -1,9 +1,7 @@
 module.exports = {
   someSidebar: {
-    ABOUT: ['relay/relaylaunch', 'relay/lpzero2relay'],
-    FAQ: ['faq/faq001','faq/faq002','faq/faq003','faq/faq004','faq/faq005','faq/faq006','faq/faq007','faq/faq008','faq/faq009','faq/faq010','faq/faq011','faq/faq012','faq/faq013','faq/faq014','faq/faq015','faq/faq016','faq/faq017','faq/faq018','faq/faq019','faq/faq020','faq/faq021','faq/faq022','faq/faq023','faq/faq024','faq/faq025','faq/faq026','faq/faq027','faq/faq028'],
-    GUIDES: ['whatiszeroexchange','howtobuyzero','zerobridge','zerogravity','fructifyzero','roadmap','partnerships','zerodefiacademy','faq', 'communities','mobile'],	  
-    BLOCKCHAINS: ['eth','avax','bsc','polygon','sol'],
+    ABOUT: ['intro', 'baas','resources'],
+    FAQ: ['faq/swapzero','faq/liquiditypools'],	  
     HELPDESK: ['sd/sd001'], 
   },
 };
