74199430ca487895b317f0e6e79cf076c4f3248e
masternodeh <hello@masternode.io>
2021-08-07 15:51:49 -0700

    Added more sidebar and helpdesk stuff back

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    ABOUT: ['intro', 'baas','resources'],
+    ABOUT: ['intro','zero-to-relay','baas','resources'],
     FAQ: ['zero-to-relay','faq/swap-zero','faq/liquidity-pools'],	  
     HELPDESK: ['sd/sd001'], 
   },
